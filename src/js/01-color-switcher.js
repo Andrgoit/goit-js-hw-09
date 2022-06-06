7bb86454b3eaa7503643fbf0f584ddd9c6ac7744
Andrgoit <stakgrom@gmail.com>
2022-06-06 11:38:58 +0300

    color-switcher

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,24 @@
+import { refs } from './01-refs';
+import { getRandomHexColor } from './01-randomGenerator';
+// тренеруюсь с импортом/экспортом
 
+let timerId;
+
+refs.startBtn.addEventListener('click', startBtnHandler);
+refs.stopBtn.addEventListener('click', stopBtnHandler);
+
+function startBtnHandler() {
+    timerId = setInterval(changeBgColor, 1000)
+    refs.startBtn.setAttribute("disabled", "disabled")
+    
+}
+
+function changeBgColor (){
+    const random = getRandomHexColor()
+    refs.body.style.backgroundColor = `${random}`;
+}
+
+function stopBtnHandler(){
+    clearInterval(timerId)
+    refs.startBtn.removeAttribute("disabled")
+}
---
+++ src/js/01-refs.js
@@ -0,0 +1,5 @@
+export const refs = {
+  body: document.body,
+  startBtn: document.querySelector('[data-start]'),
+  stopBtn: document.querySelector('[data-stop]'),
+};
